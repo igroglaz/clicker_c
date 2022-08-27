3e36300d1b6c360c4f202af5e2fd3203a381cd87
igroglaz <39904162+igroglaz@users.noreply.github.com>
2022-08-27 21:59:58 +0300

    init 1st idle test

--- 1.c
+++ 1.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
-int main(void)
+void game()
 {
-    printf("Hello world");
+    char input;
+    int ore = 0;
+
+    while (input != 'e')
+    {
+        printf("(M)ine or (e)xit\n");
+        scanf("%c", &input);
+        while (getchar() != '\n')
+            ; // clear buffer
+        if (input == 'M' || input == 'm')
+        {
+            ore++;
+            printf("You got piece of rock. You got %d rocks.\n", ore);
+        }
+    }
+
+    if (input == 'e')
+        return;
+}
+
+int main (void)
+{
+    printf( "Welcome back to Idle Tangaria! \n");
+    
+    game();
+
+    printf("Thanks for playing!\n");
+
+    getchar();
+    getchar();
     return 0;
 }
